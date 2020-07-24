8aedd7cb55c33bfe7262a255588f424b09962710
dimaematveev <dimaematveev@gmail.com>
2020-07-24 20:41:22 +0300

    Создал библиотеку для изменения/добавления записей в словарь

--- EditAddDictionary/MainWindow.xaml.cs
+++ EditAddDictionary/MainWindow.xaml.cs
@@ -9,26 +9,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BalanceMain
+namespace EditAddDictionary
 {
     /// <summary>
-    /// Interaction logic for EditDictionary.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class EditDictionary : Window
+    public partial class MainWindow : Window
     {
-        public EditDictionary()
+        public MainWindow()
         {
             InitializeComponent();
-
-            Type.Selected += Type_Selected;
-        }
-
-        private void Type_Selected(object sender, RoutedEventArgs e)
-        {
-            EditAddDictionary.MainWindow mainWindow = new EditAddDictionary.MainWindow();
-            mainWindow.ShowDialog();
         }
     }
 }
