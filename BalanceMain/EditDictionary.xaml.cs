f346c96cfd5b648cfa442fc02a2b883aadc8465c
dimaematveev <dimaematveev@gmail.com>
2020-07-24 23:05:49 +0300

    Могу добавлять!!

--- BalanceMain/EditDictionary.xaml.cs
+++ BalanceMain/EditDictionary.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -21,29 +22,76 @@ namespace BalanceMain
     {
         /// <summary> подключение к sql. Сюда отправляются запросы и получаются ответы.  </summary>
         Connect con;
+        /// <summary> заполняет DataSet данными из БД.</summary>
+        SqlDataAdapter reader;
+        DataSet ds = new DataSet();
+
         public EditDictionary(Connect con)
         {
             this.con = con;
             InitializeComponent();
 
             Add.Click += Add_Click;
+            Edit.Click += Edit_Click; ;
 
-            Type.Selected += Type_Selected;
+            Type.Selected += (s, e) => { Type_Selected(); };
+
+            Loaded += EditDictionary_Loaded;
+            
+        }
+
+        private void EditDictionary_Loaded(object sender, RoutedEventArgs e)
+        {
+            Type.IsSelected = true;
+            Type_Selected();
+            Add_Click(null, null);
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            if (ViewDictionary.DataSource == null || ViewDictionary.Rows.Count == 0)
+            {
+                return;
+            }
+            if (Type.IsSelected && ViewDictionary.SelectedCells.Count >= 1)
+            {
+                var row = ds.Tables[0].Rows[ViewDictionary.SelectedCells[0].RowIndex];
+                EditAddDictionary.DictionaryType dictionaryType = new EditAddDictionary.DictionaryType(row);
+                dictionaryType.ShowDialog();
+                if (!dictionaryType.DialogResult.Value)
+                {
+                    return;
+                }
+                SqlCommandBuilder sqlCommandBuilder = new SqlCommandBuilder(reader);
+
+                reader.Update(ds);
+                Type_Selected();
+
+            }
         }
 
         private void Add_Click(object sender, RoutedEventArgs e)
         {
             if (Type.IsSelected)
             {
-                EditAddDictionary.DictionaryType dictionaryType = new EditAddDictionary.DictionaryType();
+                EditAddDictionary.DictionaryType dictionaryType = new EditAddDictionary.DictionaryType(((DataTable)ViewDictionary.DataSource).NewRow());
                 dictionaryType.ShowDialog();
+                if (!dictionaryType.DialogResult.Value)
+                {
+                    return;
+                }
+                SqlCommandBuilder sqlCommandBuilder = new SqlCommandBuilder(reader);
+                ds.Tables[0].Rows.Add(dictionaryType.DR);
+                reader.Update(ds);
+                Type_Selected();
             }
         }
 
-        private void Type_Selected(object sender, RoutedEventArgs e)
+        private void Type_Selected()
         {
-            DataSet ds = new DataSet();
-            var reader = con.ExecuteCommand("Select * from dic.Type");
+            
+            reader = con.ExecuteCommand("Select * from dic.Type");
+            ds.Clear();
             reader.Fill(ds);
             ViewDictionary.DataSource = ds.Tables[0];
         }
--- EditAddDictionary/DictionaryType.xaml.cs
+++ EditAddDictionary/DictionaryType.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -19,9 +20,38 @@ namespace EditAddDictionary
     /// </summary>
     public partial class DictionaryType : Window
     {
-        public DictionaryType()
+        /// <summary> подключение к sql. Сюда отправляются запросы и получаются ответы.  </summary>
+        public DataRow DR { get; }
+        public DictionaryType(DataRow dr)
         {
             InitializeComponent();
+            DR = dr;
+           
+            Add.Click += Add_Click;
+            Cancel.Click += Cancel_Click;
+            Loaded += DictionaryType_Loaded;
         }
+
+        private void DictionaryType_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            GadgetName.Text = DR["GadgetName"].ToString();
+            Name.Text = DR["Name"].ToString();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            DR["GadgetName"] = GadgetName.Text;
+            DR["Name"] = Name.Text;
+            DialogResult = true;
+            Close();
+        }
+
+
     }
 }
