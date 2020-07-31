629e4295667bffcc07496dfa8777eb6f214655d9
dimaematveev <dimaematveev@gmail.com>
2020-07-31 20:56:31 +0300

    Revert "Удалил тесты"
    This reverts commit a9481c2ac9e184709817ed1cfee0007bac624326.

---
+++ EditAddDictionaryTests/DictionarySp_SiTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EditAddDictionary.Tests
+{
+    [TestClass()]
+    public class DictionarySp_SiTests
+    {
+        [TestMethod()]
+        public void DictionarySp_SiTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
