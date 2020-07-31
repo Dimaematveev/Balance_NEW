23ba0369351e2acc4e515f95477e5a73b1fc472a
dimaematveev <dimaematveev@gmail.com>
2020-07-31 21:02:26 +0300

    Revert "Удалил Test1 и TEst2"
    This reverts commit a7ca36256be9616c756756c5276b49d8b42d2524.

---
+++ TEST1/Program.cs
@@ -0,0 +1,51 @@
+﻿using Connected;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Linq;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace TEST1
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            DataContext db = new DataContext(Connect._connetionString);
+        
+            // параметры
+            List<SqlParameter> sqlParameters = new List<SqlParameter>();
+            sqlParameters.Add(new SqlParameter { ParameterName = "@ModelID", Value = 4 });
+            sqlParameters.Add(new SqlParameter { ParameterName = "@SN", Value = "M11" });
+            sqlParameters.Add(new SqlParameter { ParameterName = "@Year", Value = 2019 });
+            sqlParameters.Add(new SqlParameter { ParameterName = "@ScreenResolution", Value = "500x500" });
+            var Procedure = Connect.ExecuteProcedure("[dev].[Add_Monitor]", sqlParameters.ToArray());
+            Console.WriteLine();
+            Console.WriteLine("PROCEDURE: "+ Procedure);
+            Console.WriteLine();
+            var data = Connect.GetData("select * from dev.View_Monitor");
+            foreach (DataColumn item in data.Columns)
+            {
+                Console.Write(item.ColumnName);
+                Console.Write(" | ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(new string('-',50));
+            foreach (DataRow item in data.Rows)
+            {
+                for (int i = 0; i < data.Columns.Count; i++)
+                {
+                    Console.Write(item[i]);
+                    Console.Write(" | ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+    }
+}
