02e821aef655074fd86479c8e5578c25a0a51550
dimaematveev <dimaematveev@gmail.com>
2020-08-26 17:35:13 +0300

    Простая проверка, но надо сделать ее в ViewModel

---
+++ Balance_v3/Balance.View/ValidationRules/StringNotNullValidationRule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Balance.View.ValidationRules
+{
+    public class StringNotNullValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            bool canConvert = !string.IsNullOrWhiteSpace(value as string);
+            return new ValidationResult(canConvert, "Строка не должна быть пустой");
+        }
+    }
+}
